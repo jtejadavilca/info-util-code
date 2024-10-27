06f64d86c289a437a03a758db14426c59f177b13
Jose Tejada <jtejadavilca@gmail.com>
2024-10-27 09:32:20 +0000

    [ReactJS] Updating useForm hook

--- ReactJS/hooks/useForm.js
+++ ReactJS/hooks/useForm.js
@@ -1,7 +1,24 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
-export const useForm = (initialForm = {}, inputFocus) => {
+export const useForm = (initialForm = {}, formValidations = {}) => {
     const [formState, setFormState] = useState(initialForm);
+    const [formValidation, setFormValidation] = useState({});
+
+    useEffect(() => {
+        createValidators();
+    }, [formState]);
+
+    useEffect(() => {
+        setFormState(initialForm);
+    }, [initialForm]);
+
+    const isFormValid = useMemo(() => {
+        for (const formValue of Object.keys(formValidation)) {
+            if (formValidation[formValue] !== null) return false;
+        }
+
+        return true;
+    }, [formValidation]);
 
     const onInputChange = ({ target }) => {
         const { name, value } = target;
@@ -11,17 +28,29 @@ export const useForm = (initialForm = {}, inputFocus) => {
         });
     };
 
-    const onReset = () => {
+    const onResetForm = () => {
         setFormState(initialForm);
-        if (inputFocus?.current) {
-            inputFocus.current.focus();
+    };
+
+    const createValidators = () => {
+        const formCheckedValues = {};
+
+        for (const formField of Object.keys(formValidations)) {
+            const [fn, errorMessage] = formValidations[formField];
+
+            formCheckedValues[`${formField}Valid`] = fn(formState[formField]) ? null : errorMessage;
         }
+
+        setFormValidation(formCheckedValues);
     };
 
     return {
         ...formState,
         formState,
         onInputChange,
-        onReset,
+        onResetForm,
+
+        ...formValidation,
+        isFormValid,
     };
 };
